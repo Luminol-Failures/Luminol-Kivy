080e61463641e63a1efb150d7e298cebe256ba7e
Speak2Erase <matthew@nowaffles.com>
2021-10-02 17:03:30 -0700

    Better map drawing method without UV coordinates!
    Except this also breaks stuff :(

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from kivy.app import App
 from kivy.uix.gridlayout import GridLayout
 from src.tilemap import TileMap
 from kivy.uix.scrollview import ScrollView
+from kivy.effects.scroll import ScrollEffect
 from kivy.core.window import Window
 
 
@@ -12,18 +13,27 @@ class EditorLayout(GridLayout):
         super(EditorLayout, self).__init__(**kwargs)
 
         self.cols = 1
-        t = TileMap()
-        root = ScrollView(size_hint=(None, None), size=(Window.width, Window.height))
-        root.add_widget(t)
-        self.add_widget(root)
+        self.root = ScrollView(size_hint=(None, None), size=(Window.width, Window.height))
+        self.root.bar_width = 5
+
+        self.tilemap = TileMap()
+
+        self.root.add_widget(self.tilemap)
+
+        self.add_widget(self.root)
+    
+    def resize(self, *args):
+        self.root.size = (Window.width, Window.height)
 
 class LuminolApp(App):
 
         def __init__(self, **kwargs):
             super().__init__(**kwargs)
+            self.layout = EditorLayout()
 
         def build(self):
-            return EditorLayout()
+            Window.bind(on_resize=self.layout.resize)
+            return self.layout
 
 if __name__ == '__main__':
     LuminolApp().run()
--- src/tilemap.py
+++ src/tilemap.py
@@ -1,51 +1,62 @@
 from kivy.core.image import Texture
 from kivy.uix.widget import Widget
-from kivy.graphics import Rectangle, texture
+from kivy.graphics import (
+    RenderContext, BindTexture, Rectangle, Color
+)
 from kivy.uix.image import Image
 from src.ruby_loader import DataLoader
 
 class TileMap(Widget):
-    def __init__(self, id = 274, **kwargs):
+    def __init__(self, id = 128, **kwargs):
         super(TileMap, self).__init__(**kwargs)
 
         self.map = DataLoader().map(id)
         self.data = self.map.data
-        self.coords = []
-        self.load_coords(self.map.tileset_id)
 
         self.size_hint_x = None
         self.size_hint_y = None
         self.width = self.map.width * 32
         self.height = self.map.height * 32
 
+        self.tiles = []
+        self.load_tiles()
+        self.draw_map_tiles()
+
+
+    def load_tiles(self):
+        tileset = DataLoader().tileset(self.map.tileset_id)
+        name = f"Graphics/Tilesets/{tileset.tileset_name.decode()}.png"
+        texture = Image(source=name).texture
+        
+        self.tiles = []
+        for y in reversed(range(texture.height // 32)):
+            for x in range(texture.width // 32):
+                tile = texture.get_region(x * 32, y * 32, 32, 32)
+                self.tiles.append(tile)
+    
+    def draw_map_tiles(self, *args):
+        self.width = self.map.width * 32
+        self.height = self.map.height * 32
+
         with self.canvas:
-            #Rectangle(pos=self.pos, size=self.size)
             tileset = DataLoader().tileset(self.map.tileset_id)
-            name = f"Graphics/Tilesets/{tileset.tileset_name.decode()}.png"
+            if tileset.panorama_name.decode() != "":
+                bg_name = f"Graphics/Panoramas/{tileset.panorama_name.decode()}.png"
+                bg_texture = Image(source=bg_name).texture
+
+                bg_texture.wrap = 'repeat'
+                bg_texture.uvsize = (self.width, self.height)
+
+                Rectangle(Texture=bg_texture)
+            else:
+                Rectangle()
+
             for z in range(self.map.data.zsize - 1):
                 for y in range(self.map.height - 1):
                     for x in range(self.map.width - 1):
                         tile = self.data.xyz(x,y,z)
                         if tile > 384:
-                            coords = self.coords[tile - 384]
-                            Rectangle(source=name, pos = (x * 32, -((y - self.map.height - 1) * 32)), size = (32, 32), tex_coords = coords)
-
-    def load_coords(self, tileset_id):
-        tileset = DataLoader().tileset(tileset_id)
-        name = f"Graphics/Tilesets/{tileset.tileset_name.decode()}.png"
-        texture = Image(source=name).texture
-        for y in range(texture.height // 32):
-            for x in range(8):
-                u = 0
-                v = 0
-                if x > 0:
-                    u = (x * 32) / texture.width
-                if y > 0:
-                    v = (y * 32 + 32) / texture.height
-                w = 32 / texture.width 
-                h = -32 / texture.height
-                coords = u, v, u + w, v, u + w, v + h, u, v + h
-                self.coords.append(coords)
-
-        
-            
+                            if tile > len(self.tiles):
+                                Rectangle(texture= self.tiles[0], pos = (x * 32,-((y - self.map.height - 1) * 32 + 64)), size = (32, 32))
+                            else:
+                                Rectangle(texture= self.tiles[tile - 384], pos = (x * 32,-((y - self.map.height - 1) * 32 + 64)), size = (32, 32))
