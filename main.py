3851f5b10afb38fe4b3038e8ae4b425f3201f761
Speak2Erase <matthew@nowaffles.com>
2021-10-02 18:47:37 -0700

    Map scaling

--- main.py
+++ main.py
@@ -1,38 +1,69 @@
+from re import S
 import kivy
 from kivy.app import App
-from kivy.uix.gridlayout import GridLayout
+from kivy.uix.anchorlayout import AnchorLayout
+from kivy.uix.boxlayout import BoxLayout
 from src.tilemap import TileMap
 from kivy.uix.scrollview import ScrollView
 from kivy.effects.scroll import ScrollEffect
 from kivy.core.window import Window
+from kivy.uix.togglebutton import ToggleButton
 
-
-class EditorLayout(GridLayout):
+class EditorLayout(AnchorLayout):
 
     def __init__(self, **kwargs):
         super(EditorLayout, self).__init__(**kwargs)
 
-        self.cols = 1
-        self.root = ScrollView(size_hint=(None, None), size=(Window.width, Window.height))
+        self.anchor_x = 'left'
+        self.anchor_y = 'top'
+
+        left_layout = BoxLayout(orientation ='vertical')
+        self.root = ScrollView(size_hint_y=None, height = Window.height - 40)
         self.root.bar_width = 5
 
         self.tilemap = TileMap()
 
+        top_buttons = BoxLayout(orientation = 'horizontal')
+
+        self.topbuttons = [
+            ToggleButton(text='Scale: 1', font_size=14,  group = 'scale', state = 'down'),
+            ToggleButton(text='Scale: 2', font_size=14,  group = 'scale'),
+            ToggleButton(text='Scale: 3', font_size=14,  group = 'scale')
+        ]
+
+        self.topbuttons[0].bind(on_press=self.scale_map_1)
+        self.topbuttons[1].bind(on_press=self.scale_map_2)
+        self.topbuttons[2].bind(on_press=self.scale_map_3)
+
+        for b in self.topbuttons: top_buttons.add_widget(b)
         self.root.add_widget(self.tilemap)
 
-        self.add_widget(self.root)
+        left_layout.add_widget(top_buttons)
+        left_layout.add_widget(self.root)
+        
+        self.add_widget(left_layout)
     
+    def scale_map_1(self, *args):
+        self.tilemap.set_scale(32)
+
+    def scale_map_2(self, *args):
+        self.tilemap.set_scale(16)
+
+    def scale_map_3(self, *args):
+        self.tilemap.set_scale(8)
+
     def resize(self, *args):
-        self.root.size = (Window.width, Window.height)
+        self.root.height = Window.height - 40
 
 class LuminolApp(App):
 
         def __init__(self, **kwargs):
             super().__init__(**kwargs)
             self.layout = EditorLayout()
+            Window.bind(on_resize=self.layout.resize)
+            self.layout = self.layout
 
         def build(self):
-            Window.bind(on_resize=self.layout.resize)
             return self.layout
 
 if __name__ == '__main__':
--- src/tilemap.py
+++ src/tilemap.py
@@ -7,7 +7,7 @@ from kivy.uix.image import Image
 from src.ruby_loader import DataLoader
 
 class TileMap(Widget):
-    def __init__(self, id = 128, **kwargs):
+    def __init__(self, id = 276, **kwargs):
         super(TileMap, self).__init__(**kwargs)
 
         self.map = DataLoader().map(id)
@@ -15,18 +15,24 @@ class TileMap(Widget):
 
         self.size_hint_x = None
         self.size_hint_y = None
-        self.width = self.map.width * 32
-        self.height = self.map.height * 32
+        self.scale = 32
+
+        self.width = self.map.width * self.scale
+        self.height = self.map.height * self.scale
+
 
         self.tiles = []
         self.load_tiles()
         self.draw_map_tiles()
 
+    def set_scale(self, value):
+        self.scale = value
+        self.draw_map_tiles()
 
     def load_tiles(self):
         tileset = DataLoader().tileset(self.map.tileset_id)
         name = f"Graphics/Tilesets/{tileset.tileset_name.decode()}.png"
-        texture = Image(source=name).texture
+        texture = Image(source=name, mipmap = True).texture
         
         self.tiles = []
         for y in reversed(range(texture.height // 32)):
@@ -35,19 +41,20 @@ class TileMap(Widget):
                 self.tiles.append(tile)
     
     def draw_map_tiles(self, *args):
-        self.width = self.map.width * 32
-        self.height = self.map.height * 32
+        self.width = self.map.width * self.scale
+        self.height = self.map.height * self.scale
 
         with self.canvas:
+            self.canvas.clear()
             tileset = DataLoader().tileset(self.map.tileset_id)
             if tileset.panorama_name.decode() != "":
                 bg_name = f"Graphics/Panoramas/{tileset.panorama_name.decode()}.png"
-                bg_texture = Image(source=bg_name).texture
+                bg_texture = Image(source=bg_name, mipmap = True).texture
 
                 bg_texture.wrap = 'repeat'
-                bg_texture.uvsize = (self.width, self.height)
+                bg_texture.uvsize = (self.width / bg_texture.width, self.height / bg_texture.height)
 
-                Rectangle(Texture=bg_texture)
+                Rectangle(texture=bg_texture, size = (self.width, self.width), pos = self.pos)
             else:
                 Rectangle()
 
@@ -57,6 +64,14 @@ class TileMap(Widget):
                         tile = self.data.xyz(x,y,z)
                         if tile > 384:
                             if tile > len(self.tiles):
-                                Rectangle(texture= self.tiles[0], pos = (x * 32,-((y - self.map.height - 1) * 32 + 64)), size = (32, 32))
+                                Rectangle(
+                                        texture= self.tiles[0], 
+                                        pos = (x * self.scale,-((y - self.map.height - 1) * self.scale + self.scale * 2)), 
+                                        size = (self.scale, self.scale)
+                                    )
                             else:
-                                Rectangle(texture= self.tiles[tile - 384], pos = (x * 32,-((y - self.map.height - 1) * 32 + 64)), size = (32, 32))
+                                Rectangle(
+                                        texture= self.tiles[tile - 384], 
+                                        pos = (x * self.scale,-((y - self.map.height - 1) * self.scale + self.scale * 2)), 
+                                        size = (self.scale, self.scale)
+                                    )
