86f3e9effe24bd38cec6b14e26f1df66a9c96058
Speak2Erase <matthew@nowaffles.com>
2021-10-07 22:36:19 -0700

    Auto-tiling tileable autotiles

--- src/tilemap.py
+++ src/tilemap.py
@@ -50,9 +50,10 @@ class TileMap(Widget):
 
         self.grid = True
         self.event_graphic = True
+        self.tileset = DataLoader().tileset(self.map.tileset_id)
 
         self.layers = {}
-
+        self.autotiles = {}
         self.create_map_layer()
         self.create_event_layer()
         self.draw_layers()
@@ -60,6 +61,7 @@ class TileMap(Widget):
     def set_map_id(self, id):
         self.map = DataLoader().map(id)
         self.data = self.map.data
+        self.tileset = DataLoader().tileset(self.map.tileset_id)
 
         self.layers = {}
         self.create_map_layer()
@@ -86,11 +88,252 @@ class TileMap(Widget):
             self.create_event_layer()
         self.draw_layers()
     
+    def load_autotiles(self):
+        autotile_names = self.tileset.autotile_names
+        autotiles = []
+        self.autotiles = {}
+        for graphic in autotile_names:
+            if graphic.decode() != "":
+                autotiles.append(Image.open(f"Graphics/Autotiles/{graphic.decode()}.png"))
+            else:
+                autotiles.append(None)
+        
+        a = 1
+        blank_tile = Image.new('RGBA', (32, 32))
+        for i in range(48):
+            self.autotiles[i] = blank_tile
+        for autotile in autotiles:
+            autotile_list = []
+            if autotile == None:
+                tile = Image.new('RGBA', (32, 32))
+                for i in range(48):
+                    autotile_list.append(tile)
+            else:
+                corners = []
+                t = 0
+                for y in range(128 // 16):
+                    for x in range(96 // 16):
+                        corners.append(autotile.crop((
+                            x * 16, y * 16,
+                            x * 16 + 16, y * 16 + 16
+                        )))
+                        t += 1
+                for i in range(48):
+                    if i == 0: # 1
+                        tile = self.create_autotile(
+                            corners, 26, 27, 32, 33
+                        )
+                    elif i == 1: # 2
+                        tile = self.create_autotile(
+                            corners, 4, 27, 32, 33
+                        )
+                    elif i == 2: # 3
+                        tile = self.create_autotile(
+                            corners, 26, 5, 32, 33
+                        )
+                    elif i == 3: # 4
+                        tile = self.create_autotile(
+                            corners, 4, 5, 32, 33
+                        )
+                    elif i == 4: # 5
+                        tile = self.create_autotile(
+                            corners, 26, 27, 32, 11
+                        )
+                    elif i == 5: # 6
+                        tile = self.create_autotile(
+                            corners, 4, 27, 32, 11
+                        )
+                    elif i == 6: # 7
+                        tile = self.create_autotile(
+                            corners, 26, 5, 32, 11
+                        )
+                    elif i == 7: # 8
+                        tile = self.create_autotile(
+                            corners, 4, 5, 32, 11
+                        )
+                    elif i == 8:
+                        tile = self.create_autotile(
+                            corners, 26, 27, 10, 33
+                        )
+                    elif i == 9:
+                        tile = self.create_autotile(
+                            corners, 4, 27, 10, 33
+                        )
+                    elif i == 10:
+                        tile = self.create_autotile(
+                            corners, 26, 5, 10, 33
+                        )
+                    elif i == 11:
+                        tile = self.create_autotile(
+                            corners, 4, 5, 10, 33
+                        )
+                    elif i == 12:
+                        tile = self.create_autotile(
+                            corners, 26, 27, 10, 11
+                        )
+                    elif i == 13:
+                        tile = self.create_autotile(
+                            corners, 4, 27, 10, 11
+                        )
+                    elif i == 14:
+                        tile = self.create_autotile(
+                            corners, 26, 5, 10, 11
+                        )
+                    elif i == 15:
+                        tile = self.create_autotile(
+                            corners, 4, 5, 10, 11
+                        )
+                    elif i == 16:
+                        tile = self.create_autotile(
+                            corners, 24, 25, 30, 31
+                        )
+                    elif i == 17:
+                        tile = self.create_autotile(
+                            corners, 24, 5, 30, 31
+                        )
+                    elif i == 18:
+                        tile = self.create_autotile(
+                            corners, 24, 25, 30, 11
+                        )
+                    elif i == 19:
+                        tile = self.create_autotile(
+                            corners, 24, 5, 30, 11
+                        )
+                    elif i == 20:
+                        tile = self.create_autotile(
+                            corners, 14, 15, 20, 21
+                        )
+                    elif i == 21:
+                        tile = self.create_autotile(
+                            corners, 14, 15, 20, 11
+                        )
+                    elif i == 22:
+                        tile = self.create_autotile(
+                            corners, 14, 15, 10, 21
+                        )
+                    elif i == 23:
+                        tile = self.create_autotile(
+                            corners, 14, 15, 10, 11
+                        )
+                    elif i == 24:
+                        tile = self.create_autotile(
+                            corners, 28, 29, 34, 35
+                        )
+                    elif i == 25:
+                        tile = self.create_autotile(
+                            corners, 28, 29, 10, 35
+                        )
+                    elif i == 26:
+                        tile = self.create_autotile(
+                            corners, 4, 29, 34, 35
+                        )
+                    elif i == 27:
+                        tile = self.create_autotile(
+                            corners, 4, 29, 10, 35
+                        )
+                    elif i == 28:
+                        tile = self.create_autotile(
+                            corners, 38, 39, 44, 45
+                        )
+                    elif i == 29:
+                        tile = self.create_autotile(
+                            corners, 4, 39, 44, 45
+                        )
+                    elif i == 30:
+                        tile = self.create_autotile(
+                            corners, 38, 5, 44, 45
+                        )
+                    elif i == 31:
+                        tile = self.create_autotile(
+                            corners, 4, 5, 44, 45
+                        )
+                    elif i == 32:
+                        tile = self.create_autotile(
+                            corners, 24, 29, 30, 35
+                        )
+                    elif i == 33:
+                        tile = self.create_autotile(
+                            corners, 14, 15, 44, 45
+                        )
+                    elif i == 34:
+                        tile = self.create_autotile(
+                            corners, 12, 13, 18, 19
+                        )
+                    elif i == 35:
+                        tile = self.create_autotile(
+                            corners, 12, 13, 18, 11
+                        )
+                    elif i == 36:
+                        tile = self.create_autotile(
+                            corners, 16, 17, 22, 23
+                        )
+                    elif i == 37:
+                        tile = self.create_autotile(
+                            corners, 16, 17, 10, 23
+                        )
+                    elif i == 38:
+                        tile = self.create_autotile(
+                            corners, 40, 41, 46, 47
+                        )
+                    elif i == 39:
+                        tile = self.create_autotile(
+                            corners, 4, 41, 46, 47
+                        )
+                    elif i == 40:
+                        tile = self.create_autotile(
+                            corners, 36, 37, 42, 43
+                        )
+                    elif i == 41:
+                        tile = self.create_autotile(
+                            corners, 36, 5, 42, 43
+                        )
+                    elif i == 42:
+                        tile = self.create_autotile(
+                            corners, 12, 17, 18, 23
+                        )
+                    elif i == 43:
+                        tile = self.create_autotile(
+                            corners, 12, 13, 42, 43
+                        )
+                    elif i == 44:
+                        tile = self.create_autotile(
+                            corners, 36, 41, 42, 47
+                        )
+                    elif i == 45:
+                        tile = self.create_autotile(
+                            corners, 16, 17, 46, 47
+                        )
+                    elif i == 46:
+                        tile = self.create_autotile(
+                            corners, 12, 17, 42, 47
+                        )
+                    elif i == 47:
+                        tile = self.create_autotile(
+                            corners, 0, 1, 6, 7
+                        )
+
+
+                    autotile_list.append(tile)
+            t = 0
+            for tile in autotile_list:
+                self.autotiles[t + (a * 48)] = tile
+                t += 1
+            a += 1
+    
+    def create_autotile(self, tiles, c1, c2, c3, c4):
+        tile = Image.new('RGBA', (32, 32))
+        tile.paste(tiles[c1], (0, 0))
+        tile.paste(tiles[c2], (16, 0))
+        tile.paste(tiles[c3], (0, 16))
+        tile.paste(tiles[c4], (16, 16))
+        return tile
+
+    
     def create_map_layer(self, *args):
+        self.load_autotiles()
         grid_texture = Image.open('assets/tile_grid.png')
 
-        tileset = DataLoader().tileset(self.map.tileset_id)
-        name = f"Graphics/Tilesets/{tileset.tileset_name.decode()}.png"
+        name = f"Graphics/Tilesets/{self.tileset.tileset_name.decode()}.png"
         try:
             texture = Image.open(name)
         except FileNotFoundError:
@@ -113,6 +356,9 @@ class TileMap(Widget):
                             tile = texture.crop((tx, ty, tx + 32, ty + 32))
                             loaded_tiles[tile_id] = tile
                         layer.paste(tile, (x * 32, y * 32))
+                    else:
+                        tile = self.autotiles[tile_id]
+                        layer.paste(tile, (x * 32, y * 32))
 
             layer.save(f'temp/{z}_temp.png')
             self.layers[z] = None
@@ -130,8 +376,7 @@ class TileMap(Widget):
         events = dict(sorted(self.map.events.items(), key=lambda  item: item[1].y))
         blank_event_texture = Image.open('assets/event.png')
 
-        tileset = DataLoader().tileset(self.map.tileset_id)
-        name = f"Graphics/Tilesets/{tileset.tileset_name.decode()}.png"
+        name = f"Graphics/Tilesets/{self.tileset.tileset_name.decode()}.png"
         try:
             texture = Image.open(name)
         except FileNotFoundError:
@@ -205,17 +450,16 @@ class TileMap(Widget):
 
         self.canvas.clear()
 
-        tileset = DataLoader().tileset(self.map.tileset_id)
         with self.canvas:
-            if tileset.panorama_name.decode() != "":
-                bg_name = f"Graphics/Panoramas/{tileset.panorama_name.decode()}.png"
+            if self.tileset.panorama_name.decode() != "":
+                bg_name = f"Graphics/Panoramas/{self.tileset.panorama_name.decode()}.png"
                 try:
                     bg_image = Image.open(bg_name)
                 except FileNotFoundError:
                     bg_image = Image.open('assets/placeholder.png')
 
-                if tileset.panorama_hue != 0:
-                    hue = wrapRange(tileset.panorama_hue, 0, 359)
+                if self.tileset.panorama_hue != 0:
+                    hue = wrapRange(self.tileset.panorama_hue, 0, 359)
                     arr = np.array(np.asarray(bg_image).astype('float'))
                     bg_image = Image.fromarray(shift_hue(arr, hue / 360).astype('uint8'), 'RGBA')
                 
@@ -227,7 +471,7 @@ class TileMap(Widget):
                 bg_texture.wrap = 'repeat'
                 bg_texture.uvsize = (self.width / bg_texture.width, self.height / bg_texture.height)
                 bg_texture.flip_vertical()
-            if tileset.panorama_name.decode() != "":
+            if self.tileset.panorama_name.decode() != "":
                 Rectangle(texture=bg_texture, size = (self.map.width * self.scale, self.map.height * self.scale), pos = self.pos)
             else:
                 Color(0.10, 0.10, 0.10, 1)
@@ -251,15 +495,15 @@ class TileMap(Widget):
                     pos=self.pos
                 )
 
-        if tileset.fog_name.decode() != "":
-            fog_name = f"Graphics/Fogs/{tileset.fog_name.decode()}.png"
+        if self.tileset.fog_name.decode() != "":
+            fog_name = f"Graphics/Fogs/{self.tileset.fog_name.decode()}.png"
             try:
                 fog_image = Image.open(fog_name)
             except FileNotFoundError:
                 fog_image = Image.open('assets/placeholder.png')
 
-            if tileset.fog_hue != 0:
-                hue = wrapRange(tileset.fog_hue, 0, 359)
+            if self.tileset.fog_hue != 0:
+                hue = wrapRange(self.tileset.fog_hue, 0, 359)
                 arr = np.array(np.asarray(fog_image).astype('float'))
                 fog_image = Image.fromarray(shift_hue(arr, hue / 360).astype('uint8'), 'RGBA')
 
@@ -272,7 +516,7 @@ class TileMap(Widget):
             fog_texture.uvsize = (self.width / fog_texture.width, self.height / fog_texture.height)
             fog_texture.flip_vertical()
             with self.canvas:
-                Color(1, 1, 1, tileset.fog_opacity / 255)
+                Color(1, 1, 1, self.tileset.fog_opacity / 255)
                 Rectangle(texture=fog_texture, size = (self.map.width * self.scale, self.map.height * self.scale), pos = self.pos)
                 Color(1, 1, 1, 1)
         
